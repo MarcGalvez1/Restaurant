27f18710cad483ee8043fcb1f0795f4e901b36c4
MarcGalvez1 <marc.galvez83@gmail.com>
2024-01-07 15:49:17 -0500

    finished the home page creation

--- src/home.js
+++ src/home.js
@@ -18,9 +18,25 @@ function homeContent() {
   homeImg.classList.add("custom-img", "mx-auto", "my-3");
   homeImg.src = "./images/sanji-cooking.png";
 
+  // Create the card body
+  const bodyContainer = document.createElement("div");
+  bodyContainer.classList.add("card-body", "mx-auto");
+
+  // Create paragraph
+  const bodyParagraph = document.createElement("p");
+  bodyParagraph.classList.add(
+    "card-text",
+    "text-light",
+    "text-center",
+    "display-6"
+  );
+  bodyParagraph.innerText = "Cooked by the best chef in the New Wold";
+  bodyContainer.appendChild(bodyParagraph);
+
   // attach all child element of homeContent
   contentContainer.appendChild(titleContainer);
   contentContainer.appendChild(homeImg);
+  contentContainer.appendChild(bodyContainer);
   return { contentContainer };
 }
 export default homeContent;
