458a3669bdec7d619ed35145a806f680b0425fe5
MarcGalvez1 <marc.galvez83@gmail.com>
2024-01-11 20:59:57 -0500

    Created the menu

--- src/menu.js
+++ src/menu.js
@@ -14,30 +14,100 @@ function menuContent() {
   titleContainer.classList.add("card-header", "text-light", "mx-auto", "h1");
   titleContainer.innerText = "Menu";
 
-  //Create image
-  const homeImg = document.createElement("img");
-  homeImg.classList.add("custom-img", "mx-auto", "my-3");
-  homeImg.src = "./images/sanji-cooking.png";
-
   // Create the card body
   const bodyContainer = document.createElement("div");
   bodyContainer.classList.add("card-body", "mx-auto");
 
-  // Create paragraph
-  const bodyParagraph = document.createElement("p");
-  bodyParagraph.classList.add(
+  // Create Menu
+  // Create Chicken Sandwhich Item
+  const chickenSandwhichContainer = document.createElement("div");
+  const chickenSandwhichTitle = document.createElement("p");
+  chickenSandwhichTitle.classList.add(
     "card-text",
     "text-light",
     "text-center",
+    "display-5"
+  );
+  chickenSandwhichTitle.innerText = "Chicken Sandwhich";
+  const chickenSandwhichDesc = document.createElement("p");
+  chickenSandwhichDesc.classList.add(
+    "card-text",
+    "text-light",
+    "text-start",
     "display-6"
   );
-  bodyParagraph.innerText = "Cooked by the best chef in the New Wold";
-  bodyContainer.appendChild(bodyParagraph);
+  chickenSandwhichDesc.innerText =
+    "Succulent grilled chicken nestled in a soft bun with crisp lettuce and flavorful mayo, creating a mouthwatering and satisfying sandwich.";
 
+  const chickenSandwichHorizontalRule = document.createElement("hr");
+  chickenSandwichHorizontalRule.classList.add("custom-hr");
+
+  chickenSandwhichContainer.appendChild(chickenSandwhichTitle);
+  chickenSandwhichContainer.appendChild(chickenSandwhichDesc);
+  chickenSandwhichContainer.appendChild(chickenSandwichHorizontalRule);
+
+  // Create Hamburger menu Item
+  const hamburgerContainer = document.createElement("div");
+  const hamburgerTitle = document.createElement("p");
+  hamburgerTitle.classList.add(
+    "card-text",
+    "text-light",
+    "text-center",
+    "display-5"
+  );
+  hamburgerTitle.innerText = "Hamburger";
+  const hamburgerDesc = document.createElement("p");
+  hamburgerDesc.classList.add(
+    "card-text",
+    "text-light",
+    "text-start",
+    "display-6"
+  );
+  hamburgerDesc.innerText =
+    "Juicy beef patty embraced by a tender bun, topped with fresh lettuce, ripe tomatoes, and savory condiments for a delightful burger experience.";
+
+  const hamburgerHorizontalRule = document.createElement("hr");
+  hamburgerHorizontalRule.classList.add("custom-hr");
+
+  hamburgerContainer.appendChild(hamburgerTitle);
+  hamburgerContainer.appendChild(hamburgerDesc);
+  hamburgerContainer.appendChild(hamburgerHorizontalRule);
+
+  // Create Menu
+  // Create sloppy joes menu Item
+  const sloppyJoesContainer = document.createElement("div");
+  const sloppyJoesTitle = document.createElement("p");
+  sloppyJoesTitle.classList.add(
+    "card-text",
+    "text-light",
+    "text-center",
+    "display-5"
+  );
+  sloppyJoesTitle.innerText = "Sloppy Joes";
+  const sloppyJoesDesc = document.createElement("p");
+  sloppyJoesDesc.classList.add(
+    "card-text",
+    "text-light",
+    "text-start",
+    "display-6"
+  );
+  sloppyJoesDesc.innerText =
+    "Juicy beef patty embraced by a tender bun, topped with fresh lettuce, ripe tomatoes, and savory condiments for a delightful burger experience.";
+
+  const sloppyJoesHorizontalRule = document.createElement("hr");
+
+  sloppyJoesContainer.appendChild(sloppyJoesTitle);
+  sloppyJoesContainer.appendChild(sloppyJoesDesc);
+  sloppyJoesContainer.appendChild(sloppyJoesHorizontalRule);
+  sloppyJoesHorizontalRule.classList.add("custom-hr");
+
+  bodyContainer.appendChild(chickenSandwhichContainer);
+  bodyContainer.appendChild(hamburgerContainer);
+  bodyContainer.appendChild(sloppyJoesContainer);
   // attach all child element of homeContent
   contentContainer.appendChild(titleContainer);
-  contentContainer.appendChild(homeImg);
   contentContainer.appendChild(bodyContainer);
+
   return { contentContainer };
 }
 export default menuContent;
